b2216f2e1771d5ebfedd013cc15baf57ce04f60f
unknown <usern5949@gmail.com>
2024-02-10 19:11:54 +0530

    Added creation,deletion of todos

--- todoapp/src/App.js
+++ todoapp/src/App.js
@@ -6,6 +6,7 @@
 
 import React, { Component } from "react";
 import Todos from "./Todos";
+import AddTodo from "./AddForm";
 
 export class App extends Component {
   state = {
@@ -19,11 +20,17 @@ export class App extends Component {
     const todosList = this.state.todos.filter((todo) => todo.id !== id);
     this.setState({ todos: todosList });
   };
+  addTodo = (todo) => {
+    todo.id = Math.random();
+    let todos = [...this.state.todos, todo];
+    this.setState({ todos: todos });
+  };
   render() {
     return (
       <div className="tot-app container">
         <h1 className="center blue-text">Todo's</h1>
         <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
+        <AddTodo addTodo={this.addTodo} />
       </div>
     );
   }
